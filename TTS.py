10217e64aad79cde3404cad9ab7661b8638ddbb1
Shubham Singh <singh.shubham987@gmail.com>
2020-05-22 00:37:26 +0530

    Uses google speech recognition

--- TTS.py
+++ TTS.py
@@ -1,21 +1,29 @@
 import speech_recognition as sr 
 import pyttsx3 
+import re
 r = sr.Recognizer() 
 def SpeakText(command): 
     engine = pyttsx3.init() 
     engine.say(command) 
     engine.runAndWait() 
+def solve(x): return eval(''.join(re.findall("[+-/x]*[0-9]*",x)))
 while(1):
     try: 
         with sr.Microphone() as source2: 
-            r.adjust_for_ambient_noise(source2, duration=0.2) 
+            r.adjust_for_ambient_noise(source2, duration=0.1) 
             audio2 = r.listen(source2) 
-            MyText = r.recognize_sphinx(audio2)
-             #MyText = r.recognise_google(audio2) #remove the prev hash for better accuracy but internet is needed
-            MyText = MyText.lower() 
-            print(MyText)
-            SpeakText(MyText)
-            if MyText=='break':
+            #MyText = r.recognize_sphinx(audio2)
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+            if MyText=='close':
+                SpeakText("closing")
                 break
+            SpeakText("you said"+MyText)
+            print(MyText)
+            if MyText=='close':
+                break
+            if MyText.find('calculate')>=0:
+                SpeakText('result is'+str(solve(MyText)))
+                print(solve(MyText))
     except:
-        pass
+        SpeakText("speak again")
